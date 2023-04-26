a924f1582b45cd0f703e795817b86afa5bd4882d
Brian Faust <hello@basecode.sh>
2023-04-26 04:02:06 +0300

    first commit
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ src/ServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BombenProdukt\BladeIcons\IcoMoon;
+
+use BombenProdukt\BladeIcons\Facades\IconFamilyRegistry;
+use BombenProdukt\BladeIcons\IconFamily;
+use BombenProdukt\PackagePowerPack\Package\AbstractServiceProvider;
+
+final class ServiceProvider extends AbstractServiceProvider
+{
+    public function packageRegistered(): void
+    {
+        IconFamilyRegistry::push(
+            IconFamily::fromDirectory('icomoon', __DIR__ . '/../resources/svg'),
+        );
+    }
+}
